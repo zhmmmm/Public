e68296404fa5f35cc719b034586f37d1b13c4ae3
15982683979 <15982683979@163.com>
2018-10-06 12:22:53 +0800

    _0

--- 17_Console/Main.cpp
+++ 17_Console/Main.cpp
@@ -1,10 +1,28 @@
 #include <iostream>
 
+class Student
+{
+	int age;
+public:
+	Student()
+	{
+		age = 18;
+	}
+	void Print()
+	{
+		std::cout << "ÕÅºÆÂüÄêÁäÊÇ£º"<<age<<"Ëê£¡" << std::endl;
+	}
+};
+
 int main()
 {
-
-
+	Student *Obj;
+	Obj = new Student();
+	Obj->Print();
+	Obj->Print();
 
 	system("pause");
+	delete Obj;
+	Obj = NULL;
 	return 0;
 }
